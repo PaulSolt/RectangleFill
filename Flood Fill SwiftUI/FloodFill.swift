04eec4ffb181b2931d6057360c0a9504df5a3e0c
Paul Solt <PaulSolt@gmail.com>
2024-05-23 18:03:12 -0400

    Added 2D Canvas approach and TabViews to showcase both rendering models on the same data.

--- Flood Fill SwiftUI/FloodFill.swift
+++ Flood Fill SwiftUI/FloodFill.swift
@@ -7,22 +7,158 @@
 
 import SwiftUI
 
-class Cell: Identifiable, ObservableObject {
-    let id: UUID = UUID()
-//    let color: Color
-    @Published var filled: Bool? // nil = black, false = white, true = color
+struct FloodFill: View {
     
-    init(_ filled: Bool?) {
-        self.filled = filled
+    @ObservedObject var grid: Grid
+    
+    init(grid: Grid) {
+        self.grid = grid
     }
-}
-
-class Row: Identifiable, ObservableObject {
-    let id: UUID = UUID()
-    @Published var cells: [Cell]
     
-    init(_ cells: [Cell]) {
-        self.cells = cells
+    var body: some View {
+        
+        TabView {
+            
+            VStack {    // Rectangles with individual gesture recognizers
+                Spacer()
+                Text("Rectangle SwiftUI Grid")
+                    .font(.headline)
+                
+                rectangleGrid
+                resetButton
+                Spacer()
+            }
+            .tabItem {
+                Label("Rectangle Grid", systemImage: "square.grid.2x2")
+            }
+            
+            VStack {    // 2D coordinate math
+                Spacer()
+                Text("2D Canvas SwiftUI Grid")
+                    .font(.headline)
+                canvasGrid
+                resetButton
+                Spacer()
+            }
+            .tabItem {
+                Label("2D Canvas", systemImage: "view.2d")
+            }
+        }
+        .overlay(
+            VStack {
+                Text("Flood Fill")
+                    .font(.title)
+                Text("Two grid renders for same data. Tap to fill.")
+                    .multilineTextAlignment(.center)
+                Spacer()
+            }
+        )
+    }
+    
+    var resetButton: some View {
+        Button(action:reset, label: {
+            Text("Reset")
+        })
+    }
+    
+    var rectangleGrid: some View {
+        VStack {
+            ForEach(grid.rows) { row in
+                HStack {
+                    ForEach(row.cells) { cell in
+                        if let filled = cell.filled {
+                            
+                            Rectangle()
+                                .foregroundColor(filled ? Color.blue : Color.white)
+                                .onTapGesture {
+                                    grid.objectWillChange.send() // Force layout update
+                                    grid.floodFill(x: cell.point.x, y: cell.point.y)
+                                }
+                        } else {
+                            // border black cell
+                            Rectangle()
+                                .foregroundColor(Color.black)
+                        }
+                        
+                    }
+                }
+            }
+        }
+        .aspectRatio(contentMode: .fit)
+        .padding(8)
+        .background(Color.init(white: 0.9))
+    }
+    
+    @State var canvasSize: CGSize = CGSize.zero
+    
+    // Alternate canvas view - uses cellAtPoint(x:,y:) logic
+    var canvasGrid: some View {
+        Canvas { context, size in
+            let spacing: CGFloat = 8
+            let width = (size.width - spacing * CGFloat(grid.size + 1)) / CGFloat(grid.size)
+            
+            for y in 0 ..< grid.size {
+                for x in 0 ..< grid.size {
+                    let x1 = CGFloat(x) * (width + spacing) + spacing
+                    let y1 = CGFloat(y) * (width + spacing) + spacing
+                    let frame = CGRect(x: x1, y: y1, width: width, height: width)
+                    
+                    var color: Color = .black
+                    
+                    if let filled = grid.rows[y].cells[x].filled {
+                        color = filled ? .blue : .white
+                    }
+                    context.fill(Rectangle().path(in: frame), with: .color(color))
+                }
+            }
+        }
+        .overlay( // Measure size for cellAtPoint()
+            GeometryReader { proxy in
+                Color.clear
+                    .onAppear() {
+                        canvasSize = proxy.size
+                    }
+            }
+        )
+        .background(Color(white: 0.9))
+        .aspectRatio(contentMode: .fit)
+        .onTapGesture { location in
+            let point = cellAtPoint(location)
+            print("Cell: \(point))")
+            grid.floodFill(x: point.x, y: point.y)
+            grid.objectWillChange.send()
+        }
+    }
+    
+    // Determine touch point, grid top left edges are considered part of
+    // pixel for touch input so that we always get visual feedback when tapping
+    func cellAtPoint(_ point: CGPoint) -> (x: Int, y: Int) {
+        var xPos: CGFloat = 0
+        var yPos: CGFloat = 0
+        var xOut: Int = 0
+        var yOut: Int = 0
+        let cellWidth = canvasSize.width / CGFloat(grid.size) // Treat top left edge as part of grid
+        
+        for i in 0..<grid.size {
+            if point.x >= xPos && point.x < xPos + cellWidth {
+                xOut = i
+                break
+            }
+            xPos += cellWidth
+        }
+        
+        for i in 0..<grid.size {
+            if point.y >= yPos && point.y < yPos + cellWidth {
+                yOut = i
+                break
+            }
+            yPos += cellWidth
+        }
+        return (Int(xOut), Int(yOut))
+    }
+    
+    func reset() {
+        grid.reset()
     }
 }
 
@@ -40,93 +176,66 @@ class Grid: Identifiable, ObservableObject {
     func generateBoard(size: Int) -> [Row] {
         var newGrid = [Row]()
         // randomly choose not filled or empty (nil)
-        for _ in 0 ..< size {
+        for y in 0 ..< size {
             var cells = [Cell]()
-            for _ in 0 ..< size {
+            for x in 0 ..< size {
                 let isBorder = Int.random(in: 0...1)
-                cells.append(Cell(isBorder == 0 ? nil : false))
+                cells.append(Cell(isBorder == 0 ? nil : false, point: (x: x, y: y)))
             }
             newGrid.append(Row(cells))
         }
         return newGrid
     }
     
-    func floodFill() {
+    func reset() {
+        rows = generateBoard(size: size)
+    }
+    
+    func floodFill(x: Int, y: Int) {
+        if isValidCell(x: x, y: y) {
+            rows[y].cells[x].filled = true
+        }
         
-       // Look in each direction
-        // if isValid
-            // fill the cell,
-            // take a step in that direction
-        
-        
+        // Recursive search for neighbors
+        if isValidCell(x: x, y: y - 1) { // up
+            floodFill(x: x, y: y - 1)
+        }
+        if isValidCell(x: x + 1, y: y) { // right
+            floodFill(x: x + 1, y: y)
+        }
+        if isValidCell(x: x, y: y + 1) { // down
+            floodFill(x: x, y: y + 1)
+        }
+        if isValidCell(x: x - 1, y: y) { // left
+            floodFill(x: x - 1, y: y)
+        }
     }
     
     func isValidCell(x: Int, y: Int) -> Bool {
-        
-        guard x >= 0 || y >= 0 || x < size || y < size else { return false }
-        
-//        let row = rows[y]
-//        let cell = row[x]
-//        return cell.
-        return false
+        guard x >= 0 && y >= 0 && x < size && y < size else { return false }
+        return rows[y].cells[x].filled == false
     }
-    
 }
 
-struct FloodFill: View {
+class Cell: Identifiable, ObservableObject {
+    let id: UUID = UUID()
+    let point: (x: Int, y: Int)
     
-    @ObservedObject var grid: Grid
-//    = [
-//        Row([Cell(false), Cell(nil), Cell(true)]),
-//        Row([Cell(false), Cell(nil), Cell(true)]),
-//        Row([Cell(false), Cell(nil), Cell(true)]),
-//    ]
+    @Published var filled: Bool?
     
-    init(grid: Grid) {
-        self.grid = grid
+    init(_ filled: Bool?, point: (x: Int, y: Int)) {
+        self.filled = filled
+        self.point = point
     }
-    
-    var body: some View {
-        VStack {
-            Spacer()
-            
-            VStack {
-                ForEach(grid.rows) { row in
-                    HStack {
-                        ForEach(row.cells) { cell in
-                            if let filled = cell.filled {
-                                
-                                Rectangle()
-                                    .foregroundColor(filled ? Color.blue : Color.white)
-                                    .onTapGesture {
-                                        print("Tap")
-                                        
-//                                        if cell
-                                        cell.filled = true
-//                                        print(cell.filled)
-                                        grid.objectWillChange.send()
+}
 
-                                        // TODO: fill the remaining
-                                    }
-                                
-                            } else {
-                                // border black cell
-                                Rectangle()
-                                    .foregroundColor(Color.black)
-                            }
-                            
-                        }
-                    }
-                }
-            }
-            .aspectRatio(contentMode: .fit)
-            .background(Color.init(white: 0.9))
-            Spacer()
-        }
-        .background(Color.green)
+class Row: Identifiable, ObservableObject {
+    let id: UUID = UUID()
+    @Published var cells: [Cell]
+    
+    init(_ cells: [Cell]) {
+        self.cells = cells
     }
-    
-    
 }
 
 #Preview {
